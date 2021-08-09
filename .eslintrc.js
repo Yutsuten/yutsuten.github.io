9c0946aa8536d532f0d904c10638a5b9de2f4461
Yutsuten <mateus.etto@gmail.com>
2021-08-09 11:42:38 +0900

    Update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,33 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es6": true,
-    "node": true
+  'env': {
+    'browser': true,
+    'es6': true,
+    'node': true
   },
-  "extends": "eslint:recommended",
-  "parserOptions": {
-    "sourceType": "module"
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+    'sourceType': 'module'
   },
-  "globals": {
-    "Vue": true
+  'globals': {
+    'Vue': true
   },
-  "rules": {
-    "indent": [
-      "error",
+  'rules': {
+    'indent': [
+      'error',
       2,
-      { "SwitchCase": 1 }
+      { 'SwitchCase': 1 }
     ],
-    "linebreak-style": [
-      "error",
-      "unix"
+    'linebreak-style': [
+      'error',
+      'unix'
     ],
-    "quotes": [
-      "error",
-      "single"
+    'quotes': [
+      'error',
+      'single'
     ],
-    "semi": [
-      "error",
-      "never"
+    'semi': [
+      'error',
+      'always'
     ]
   }
 };
--- assets/notes.js
+++ assets/notes.js
@@ -1,56 +1,56 @@
 class IndexTree {
   constructor () {
-    this.root = {children: []}
+    this.root = {children: []};
   }
 
   addNode (path) {
-    let currentNode, text, isLeaf, childNode, href
-    currentNode = this.root
+    let currentNode, text, isLeaf, childNode, href;
+    currentNode = this.root;
     path.split('/').forEach(function (subPath, index, pathArray) {
-      text = subPath.charAt(0).toUpperCase() + subPath.slice(1).replace(/_/g, ' ')
-      isLeaf = index === pathArray.length - 1
+      text = subPath.charAt(0).toUpperCase() + subPath.slice(1).replace(/_/g, ' ');
+      isLeaf = index === pathArray.length - 1;
       if (isLeaf) {
-        childNode = null
+        childNode = null;
       } else {
-        childNode = currentNode.children.find(node => node.text === text)
+        childNode = currentNode.children.find(node => node.text === text);
       }
       if (!childNode) {
-        href = isLeaf ? `#${path.replace(/\//g, '-')}` : null
-        childNode = {text: text, href: href, children: []}
-        currentNode.children.push(childNode)
+        href = isLeaf ? `#${path.replace(/\//g, '-')}` : null;
+        childNode = {text: text, href: href, children: []};
+        currentNode.children.push(childNode);
       }
-      currentNode = childNode
-    })
+      currentNode = childNode;
+    });
   }
 }
 
 
 function getFilteredIndexList (indexList, search) {
   if (search) {
-    let keywords = search.replace(/[.*+?^${}()|[\]\\]/g, '\\$&').split(' ')
-    let filter = keywords.map(keyword => `(?=.*${keyword})`).join('')
-    let filterRegex = new RegExp(`^${filter}.*$`, 'i')
-    return indexList.filter(index => filterRegex.test(index))
+    let keywords = search.replace(/[.*+?^${}()|[\]\\]/g, '\\$&').split(' ');
+    let filter = keywords.map(keyword => `(?=.*${keyword})`).join('');
+    let filterRegex = new RegExp(`^${filter}.*$`, 'i');
+    return indexList.filter(index => filterRegex.test(index));
   }
-  return indexList
+  return indexList;
 }
 
 function generateIndex (indexTree, depth) {
   /* Process the index tree into an index list */
-  let indexList = []
+  let indexList = [];
   indexTree.forEach((index) => {
     indexList.push({
       text: index.text,
       link: index.href,
       depth: depth,
-    })
+    });
     if (index.children.length) {
       indexList = indexList.concat(
         generateIndex(index.children, depth + 1)
-      )
+      );
     }
-  })
-  return indexList
+  });
+  return indexList;
 }
 
 new Vue({
@@ -65,76 +65,76 @@ new Vue({
   },
   methods: {
     openNote: function (click) {
-      let targetHash = click.target.hash
+      let targetHash = click.target.hash;
       if (targetHash) {
-        this.activeNote = targetHash.substr(1)
+        this.activeNote = targetHash.substr(1);
       }
       if (window.innerWidth < 768) {
-        this.$root.$emit('bv::toggle::collapse', 'index')
+        this.$root.$emit('bv::toggle::collapse', 'index');
       }
     },
     showMenu: function () {
       if (!this.menuShown) {
-        this.$root.$emit('bv::toggle::collapse', 'index')
+        this.$root.$emit('bv::toggle::collapse', 'index');
       }
     },
     keydown: function (e) {
-      let ctrl = e.ctrlKey
-      let enter = e.keyCode === 13
-      let backspace = e.keyCode === 8
-      let slash = e.keyCode === 191
-      let space = e.keyCode === 32
-      let letter = e.keyCode >= 65 && e.keyCode <= 90
+      let ctrl = e.ctrlKey;
+      let enter = e.keyCode === 13;
+      let backspace = e.keyCode === 8;
+      let slash = e.keyCode === 191;
+      let space = e.keyCode === 32;
+      let letter = e.keyCode >= 65 && e.keyCode <= 90;
       if (!ctrl && (backspace || slash || space || letter)) {
-        document.getElementById('search').focus()
+        document.getElementById('search').focus();
       }
       if (document.activeElement.id === 'search' && document.activeElement.value.trim().length && enter) {
-        let index = document.getElementById('index')
-        let links = index.getElementsByClassName('link')
+        let index = document.getElementById('index');
+        let links = index.getElementsByClassName('link');
         if (links.length) {
-          links[0].click()
+          links[0].click();
         }
       }
     },
     searchUpdated: function () {
       // Update URL with the new search text
-      let url = new URL(window.location.href)
-      let search = this.search.trim().replace(/\s+/g, ' ')
+      let url = new URL(window.location.href);
+      let search = this.search.trim().replace(/\s+/g, ' ');
       if (search) {
-        url.searchParams.set('search', search)
+        url.searchParams.set('search', search);
       } else {
-        url.searchParams.delete('search')
+        url.searchParams.delete('search');
       }
-      window.history.replaceState({}, null, url.href)
+      window.history.replaceState({}, null, url.href);
 
-      this.updateIndexTree()
+      this.updateIndexTree();
     },
     collapseStateChanged: function (collapseId, isJustShown) {
-      this.menuShown = isJustShown
+      this.menuShown = isJustShown;
     },
     updateIndexTree: function () {
       let filteredIndexList = getFilteredIndexList(
         this.rawIndexList,
         this.search.trim().replace(/\s+/g, ' ')
-      )
+      );
 
-      let indexTree = new IndexTree()
-      filteredIndexList.forEach(path => indexTree.addNode(path))
+      let indexTree = new IndexTree();
+      filteredIndexList.forEach(path => indexTree.addNode(path));
 
-      this.indexList = generateIndex(indexTree.root.children, 0)
+      this.indexList = generateIndex(indexTree.root.children, 0);
     }
   },
   mounted: function () {
-    let rawIndexDataElement = document.getElementById('raw-index-data')
-    this.rawIndexList = JSON.parse(rawIndexDataElement.textContent)
-    rawIndexDataElement.remove()
+    let rawIndexDataElement = document.getElementById('raw-index-data');
+    this.rawIndexList = JSON.parse(rawIndexDataElement.textContent);
+    rawIndexDataElement.remove();
 
-    window.addEventListener('keydown', this.keydown)
-    this.$root.$on('bv::collapse::state', this.collapseStateChanged)
+    window.addEventListener('keydown', this.keydown);
+    this.$root.$on('bv::collapse::state', this.collapseStateChanged);
 
-    let search = new URL(window.location.href).searchParams.get('search')
-    this.search = search ? search : ''
+    let search = new URL(window.location.href).searchParams.get('search');
+    this.search = search ? search : '';
 
-    this.updateIndexTree()
+    this.updateIndexTree();
   }
-})
+});
